4320faf9e897dcf2d6f0588995782557545f059d
Amritha-07 <p.p.amrithakumar@gmail.com>
2023-09-20 14:52:59 +0530

    ADD: header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
